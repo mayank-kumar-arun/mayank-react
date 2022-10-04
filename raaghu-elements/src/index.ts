928f953d0447fac5a810c97bbc2713b3778093a2
Alok Nakhate <alok.nakhate@waiin.com>
2022-10-04 19:28:18 +0530

    Update index.ts

--- raaghu-elements/src/index.ts
+++ raaghu-elements/src/index.ts
@@ -4,16 +4,9 @@ export {default as RdsInput} from './rds-input';
 export {default as RdsCheckbox} from './rds-checkbox';
 export {default as RdsCheckboxParentChild} from './rds-checkbox-parent-child';
 export {default as RdsAvatar} from './rds-avatar';
-<<<<<<< Updated upstream
 export {default as RdsLabel} from './rds-label';
 export {default as RdsColorPicker} from './rds-color-picker';
-<<<<<<< HEAD
-=======
-export {default as RdsColorPicker} from './rds-color-picker';
 export {default as RdsAddressDetail} from './rds-address-detail';
->>>>>>> Stashed changes
-=======
 export {default as RdsBreadcrumb} from './rds-breadcrumb';
 export {default as RdsSelectList} from './rds-select-list';
 export {default as RdsDowndrop} from './rds-dropdown';
->>>>>>> 74601dbc2d223b0db39b71d20cfe2fb83af84336
