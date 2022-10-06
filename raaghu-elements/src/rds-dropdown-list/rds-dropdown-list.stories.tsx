6f1015ae6f8efd07b2b38294ddcdfc1e3e2414f1
Siddharth Pal <109146967+SIDDHARTHPAL223427@users.noreply.github.com>
2022-10-06 18:37:24 +0530

    Added Dropdown List element

--- raaghu-elements/src/rds-dropdown-list/rds-dropdown-list.stories.tsx
+++ raaghu-elements/src/rds-dropdown-list/rds-dropdown-list.stories.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import RdsDropdown from "./rds-dropdown"
+import RdsDropdownList from "./rds-dropdown-list";
 
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Elements/Dropdown",
-  component: RdsDropdown,
+  title: "Elements/DropdownList",
+  component: RdsDropdownList,
   argTypes: {
     size: {
         options: ["small","mid","large"],
@@ -25,22 +25,16 @@ export default {
         control: { type: "radio" },
       }
   },
-  decorators: [
-    (Story) => (
-      <div style={{ margin:"150px 0", textAlign:"center" }}>
-        <Story />
-      </div>
-    ),
-  ],
-} as ComponentMeta<typeof RdsDropdown>;
+  
+} as ComponentMeta<typeof RdsDropdownList>;
 
 
-const Template: ComponentStory<typeof RdsDropdown> = (args) => <RdsDropdown {...args} />;
+const Template: ComponentStory<typeof RdsDropdownList> = (args) => <RdsDropdownList {...args} />;
 
 export const Default = Template.bind({});
 
 Default.args = {
-    colorVariant: "primary",
+    placeholder:"Filter",
     size: "mid",
     darkDropdown:false,
     label:"Dropdown Button",
@@ -66,32 +60,4 @@ Default.args = {
    
 };
 
-export const WithSplit = Template.bind({});
-
-WithSplit.args = {
-    colorVariant: "primary",
-    size: "mid",
-    darkDropdown:false,
-    label:"Dropdown Button",
-    direction:"Drop-Down",
-    role:"Dropdown-Button with split",
-    listItems: [
-      {
-        value: "Export To Excel",
-        id: "1",
-        href: "",
-      },
-      {
-        value: "Import From Excel",
-        id: "2",
-        href: "",
-      },
-      {
-        value: "Click here download sample import file",
-        id: "3",
-        href: "",
-      },
-    ],
-   
-};
 
