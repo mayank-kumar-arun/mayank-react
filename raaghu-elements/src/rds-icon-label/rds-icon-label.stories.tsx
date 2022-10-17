63faf18d6900fdfb1c582e7a98166e02cf206864
Abhishek Gaurav <109716529+agaurav0019@users.noreply.github.com>
2022-10-17 19:19:42 +0530

    Added Flags icons and added Icon label Elements

---
+++ raaghu-elements/src/rds-icon-label/rds-icon-label.stories.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import RdsIconLabel from './rds-icon-label'
+
+export default {
+    title: "Elements/Icon Label",
+    component: RdsIconLabel,
+    argTypes:{
+        size: {
+            options: ["small", "medium", "large"],
+            control: { type: "select" },
+          },
+          iconSize: {
+            options: ["small", "medium", "large"],
+            control: { type: "select" },
+          },
+    }
+   
+  } as ComponentMeta<typeof RdsIconLabel>;
+
+  const Template: ComponentStory<typeof RdsIconLabel> = (args) => (
+    <RdsIconLabel {...args} />
+  );
+
+export const Default = Template.bind({});
+Default.args = {
+    label:'',
+    icon: '',
+    size:'small',
+    iconSize:"small",
+}
+  
--- raaghu-elements/src/rds-icon/rds-icon.tsx
+++ raaghu-elements/src/rds-icon/rds-icon.tsx
@@ -1,33 +1,35 @@
 import React from "react";
 import { Icons } from "./Icons";
+import { Flags } from "./flag-icons";
 
 export interface RdsIconProps {
-  width: string;
-  height: string;
-  color: string;
+  width?: string;
+  height?: string;
+  color?: string;
   name: string;
+  fill?: boolean;
+  stroke?: boolean;
 }
 
 const RdsIcon = (props: RdsIconProps) => {
-
-  let icon= Icons[props.name];
+  let fillProps = props.fill ? "black" : "none";
+  let strokeProps = props.stroke ? "black" : "none";
+  let name:string = (props.name).toLowerCase();
+  let icon= Icons.hasOwnProperty(name) ? Icons[name] : Flags[name];
 
   return (
     <>
 
-
-     
-     <div style={{
+     <svg style={{
       width: props.width ,
       height: props.height,
+      fill: fillProps,
+      stroke: strokeProps || "black",
       
      }}
       dangerouslySetInnerHTML={{__html: icon }}
-      ></div>
-     
+      ></svg>
 
-
-      
     </>
   );
 };
