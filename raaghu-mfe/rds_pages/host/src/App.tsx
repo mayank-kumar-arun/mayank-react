9c770f9496d1496b1b8293617372aa078a646d5a
Alok Nakhate <alok.nakhate@waiin.com>
2022-10-17 19:15:32 +0530

    Update App.tsx

--- raaghu-mfe/rds_pages/host/src/App.tsx
+++ raaghu-mfe/rds_pages/host/src/App.tsx
@@ -2,35 +2,12 @@ import { Link, Route, Routes } from "react-router-dom";
 import React, { Suspense } from "react";
 import Main from "./Main";
 
-
-import Login from '../../login/src/Login/Login'
-
 const App = () => {
 	return (
-		
-
       <div>
-        {/* <ul>
-        
-          <li><Link to='/Dashboard'>Dashboard</Link></li>
-          <li><Link to='/'>Login</Link></li>
-        </ul>
-        <hr /> */}
-
-
         <Main />     
-
-
       </div>  
-		
-		
-		
-
 	)
-
 }
-
-	
-
 
 export default App;
