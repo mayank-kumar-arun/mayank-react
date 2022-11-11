9daf033cb6b5358dcfce5a36a1302293a24b86ca
Amardeep Phule <amardeepphule@gmail.com>
2022-11-11 11:39:54 +0530

    Revert "latest"
    This reverts commit c311f2d6a22ec14f14106eb80bcf8c7934167581.

--- raaghu-mfe/rds_components/src/App.tsx
+++ raaghu-mfe/rds_components/src/App.tsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import RdsCompAdminDashboard from "./admin-dashboard/rds-comp-admin-dashboard";
 import RdsCompForgotPassword from "./forgot-password/rds-comp-forgot-password";
@@ -8,21 +9,21 @@ import RdsCompTopNavigation from "./top-navigation/rds-comp-top-navigation";
 import RdsCompWebsiteLog from "./website-log/rds-comp-website-log";
 import RdsCompAlertPopup from "./alert-popup/rds-comp-alert-popup";
 const App = () => {
-  const loginHandler = (email?: string, password?: string) => {
-    console.log(email);
-    console.log(password);
-  };
-  return (
+    const loginHandler =(email?:string,password?:string)=>{
+        console.log(email);
+        console.log(password);
+    }
+    return (
     <div>
-      <RdsCompLogin onLogin={loginHandler}></RdsCompLogin>
-      <RdsCompAdminDashboard></RdsCompAdminDashboard>
-      <RdsCompSideNavigation></RdsCompSideNavigation>
-      <RdsCompTopNavigation></RdsCompTopNavigation>
-      <RdsCompForgotPassword></RdsCompForgotPassword>
-      <RdsCompWebsiteLog></RdsCompWebsiteLog>
-      <RdsCompAlertPopup />
-      <RdsCompOrganizationTree></RdsCompOrganizationTree>
+        <RdsCompLogin  onLogin={loginHandler}></RdsCompLogin>
+        <RdsCompAdminDashboard></RdsCompAdminDashboard>
+        <RdsCompSideNavigation></RdsCompSideNavigation>
+        <RdsCompTopNavigation></RdsCompTopNavigation>
+        <RdsCompForgotPassword></RdsCompForgotPassword>
+        <RdsCompWebsiteLog></RdsCompWebsiteLog>
+        <RdsCompAlertPopup/>
+        <RdsCompOrganizationTree></RdsCompOrganizationTree>
     </div>
-  );
-};
+    )
+    };
 export default App;
