c311f2d6a22ec14f14106eb80bcf8c7934167581
Amardeep Phule <amardeepphule@gmail.com>
2022-11-11 11:31:10 +0530

    latest

--- raaghu-mfe/rds_components/src/App.tsx
+++ raaghu-mfe/rds_components/src/App.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import RdsCompAdminDashboard from "./admin-dashboard/rds-comp-admin-dashboard";
 import RdsCompForgotPassword from "./forgot-password/rds-comp-forgot-password";
@@ -9,21 +8,21 @@ import RdsCompTopNavigation from "./top-navigation/rds-comp-top-navigation";
 import RdsCompWebsiteLog from "./website-log/rds-comp-website-log";
 import RdsCompAlertPopup from "./alert-popup/rds-comp-alert-popup";
 const App = () => {
-    const loginHandler =(email?:string,password?:string)=>{
-        console.log(email);
-        console.log(password);
-    }
-    return (
+  const loginHandler = (email?: string, password?: string) => {
+    console.log(email);
+    console.log(password);
+  };
+  return (
     <div>
-        <RdsCompLogin  onLogin={loginHandler}></RdsCompLogin>
-        <RdsCompAdminDashboard></RdsCompAdminDashboard>
-        <RdsCompSideNavigation></RdsCompSideNavigation>
-        <RdsCompTopNavigation></RdsCompTopNavigation>
-        <RdsCompForgotPassword></RdsCompForgotPassword>
-        <RdsCompWebsiteLog></RdsCompWebsiteLog>
-        <RdsCompAlertPopup/>
-        <RdsCompOrganizationTree></RdsCompOrganizationTree>
+      <RdsCompLogin onLogin={loginHandler}></RdsCompLogin>
+      <RdsCompAdminDashboard></RdsCompAdminDashboard>
+      <RdsCompSideNavigation></RdsCompSideNavigation>
+      <RdsCompTopNavigation></RdsCompTopNavigation>
+      <RdsCompForgotPassword></RdsCompForgotPassword>
+      <RdsCompWebsiteLog></RdsCompWebsiteLog>
+      <RdsCompAlertPopup />
+      <RdsCompOrganizationTree></RdsCompOrganizationTree>
     </div>
-    )
-    };
+  );
+};
 export default App;
