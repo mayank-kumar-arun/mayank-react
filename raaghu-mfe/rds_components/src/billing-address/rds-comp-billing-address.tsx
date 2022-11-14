90c04f67e1ba6ad6ea918e66f5a56d5a000aa0ee
anshuman-singh-wai <112934036+anshuman-singh-wai@users.noreply.github.com>
2022-11-14 19:25:34 +0530

    Enhanced RdsBillingAddress and did some required feature enhancement in RdsInput

--- raaghu-mfe/rds_components/src/billing-address/rds-comp-billing-address.tsx
+++ raaghu-mfe/rds_components/src/billing-address/rds-comp-billing-address.tsx
@@ -1,86 +1,129 @@
 
+import { useState } from 'react'
 import { RdsInput, RdsButton, RdsSelectList } from '../rds-elements'
 
-export interface RdsCompBillingAddressProps { 
-    countryList: {option:string}[],
-    IndianStateList: {option:string}[]
+export interface RdsCompBillingAddressProps {
+    countryList: { option: string }[],
+    IndianStateList: { option: string }[]
 }
+
+// export interface RdsCompBillingAddressErrorMessages {
+//     FirstNameErrorMessage: string,
+//     LastNameErrorMessage: string,
+//     EmailErrorMessage: string,
+
+// }
 
 const RdsCompBillingAddress = (props: RdsCompBillingAddressProps) => {
 
-    const onSubmitHandler = (e: any) => { }
+    const onSubmitHandler = (e: any) => {
+        e.preventDefault()
+        console.log(e)
+    }
+
+
+    //Regarding Validations
+
+    const [firstNameErrorMessage, setFirstNameErrorMessage] = useState<string>("")
+    const [lastNameErrorMessage, setLastNameErrorMessage] = useState<string>("")
+    const [companyErrorMessage, setCompanyErrorMessage] = useState<string>("")
+    const [phoneErrorMessage, setPhoneErrorMessage] = useState<string>("")
+    const [addressErrorMessage, setAddressErrorMessage] = useState<string>("")
+
+    const phoneValidationHandler = (e: any) => {
+        if (e.target.value.trim() === "")
+            setPhoneErrorMessage("Phone is required")
+        else if (!isNaN(e.target.value.trim()) || (e.target.value.trim() != e.target.value) )
+            setPhoneErrorMessage("Enter valid phone number")
+        else if (e.target.value.Includes("e"))
+            setPhoneErrorMessage("Enter valid phone number")
+        else
+            setPhoneErrorMessage("")
+    }
+
 
 
     return (
         <>
+
+
             <div className="contact-information">
+
                 <h3 className="pb-2">Billing Address</h3>
+
                 <form id="billingAddressForm" onSubmit={onSubmitHandler}>
+
                     <div className="row">
+
                         <div className="col-md-6 mb-3">
                             <RdsInput label="First Name" titleType="top" size="medium" inputType="text" name="firstName"
                                 // isRequired={true}  
-                                placeholder="Enter First Name" 
-                                // required 
-                                />
-                            {/* <div className="form-control-feedback" *ngIf="firstName.errors&&(firstName.dirty||firstName.touched)">
-                    <span className="text-danger" *ngIf="firstName.errors.required">{{
-                        "First Name is
-                        required"|translate}}</span>
-                </div> */}
+                                placeholder="Enter First Name"
+                                required
+                                onBlur={(e) => { e.target.value === "" ? setFirstNameErrorMessage("First Name is required") : setFirstNameErrorMessage("") }}
+                            />
+                            {firstNameErrorMessage != "" && <div className="form-control-feedback">
+                                <span className="text-danger">{firstNameErrorMessage}</span>
+                            </div>}
                         </div>
+
                         <div className="col-md-6 mb-3">
                             <RdsInput label="Last Name" titleType="top" size="medium" inputType="text" name="lastName"
                                 //  [isRequired]="true" 
                                 // required
-                                placeholder="Enter Last Name" />
-                            {/* <div className="form-control-feedback" *ngIf="lastName.errors&&(lastName.dirty||lastName.touched)">
-                            <span className="text-danger" *ngIf="lastName.errors.required">{{
-                                "Last Name is
-                        required"|translate}}</span>
-                </div> */}
+                                placeholder="Enter Last Name"
+                                onBlur={(e) => { e.target.value === "" ? setLastNameErrorMessage("Last Name is required") : setLastNameErrorMessage("") }}
+
+                            />
+                            {lastNameErrorMessage != "" && <div className="form-control-feedback">
+                                <span className="text-danger">{lastNameErrorMessage}</span>
+                            </div>}
                         </div >
+
                     </div >
+
                     <div className="row">
+
                         <div className="col-md-6 mb-3">
                             <RdsInput label="Company" titleType="top" size="medium" name="company" id="txtCompany" placeholder="Enter Company" />
                         </div>
                         <div className="col-md-6 mb-3">
-                            <RdsInput label="Phone" titleType="top" 
-                            // isRequired={true} 
-                            size="medium" name="phone"
-                                id="txtPhone" 
+                            <RdsInput label="Phone" titleType="top"
+                                // isRequired={true} 
+                                size="medium" name="phone"
+                                id="txtPhone"
                                 // isRequired={true} 
                                 // required
-                                placeholder="Enter phone" />
-                            {/* <div className="form-control-feedback" * ngIf="phone.errors&&(phone.dirty||phone.touched)" >
-                    <span className="text-danger" * ngIf="phone.errors.required" > {{
-                        "Phone Number is
-                        required"|translate}}</span>
-                </div > */}
+                                placeholder="Enter phone"
+                                onBlur={phoneValidationHandler}
+                            />
+                            {phoneErrorMessage != "" && <div className="form-control-feedback">
+                                <span className="text-danger">{phoneErrorMessage}</span>
+                            </div>}
                         </div >
                     </div >
                     <div className="row">
                         <div className="col-md-12 mb-3">
-                            <RdsInput label="Address" titleType="top" 
-                            // isRequired={true} 
-                            size="medium" name="address"
-                                id="txtAddress" 
-                                // required 
-                                />
-                            {/* <div className="form-control-feedback" *ngIf="address.errors&&(address.dirty||address.touched)">
-                        <span className="text-danger" *ngIf="address.errors.required">{{ "address is required"| translate }}</span>
-            </div > */}
+                            <RdsInput label="Address" titleType="top"
+                                // isRequired={true} 
+                                size="medium" name="address"
+                                id="txtAddress"
+                                onBlur={(e) => { e.target.value === "" ? setAddressErrorMessage("Last Name is required") : setAddressErrorMessage("") }}
+                            // required 
+                            />
+                            {addressErrorMessage != "" && <div className="form-control-feedback">
+                                <span className="text-danger">{addressErrorMessage}</span>
+                            </div>}
                         </div >
                     </div >
                     <div className="row">
                         <div className="col-md-6 mb-3">
-                            <RdsInput label="City" titleType="top" 
-                            // isRequired={true} 
-                            size="medium" name="city"
-                                id="txtCompany" placeholder="Enter City" 
-                                // required 
-                                />
+                            <RdsInput label="City" titleType="top"
+                                // isRequired={true} 
+                                size="medium" name="city"
+                                id="txtCompany" placeholder="Enter City"
+                            // required 
+                            />
                             {/* <div className="form-control-feedback" *ngIf="city.errors && (city.dirty || city.touched)">
 
         <span className="text-danger" *ngIf="city.errors.required">City is required</span>
@@ -116,8 +159,8 @@ const RdsCompBillingAddress = (props: RdsCompBillingAddressProps) => {
                                 // (change)="SelectState($event)" 
                                 // name="countryInfo" 
                                 // required 
-                                selectItems={props.IndianStateList} 
-                                />
+                                selectItems={props.IndianStateList}
+                            />
                             {/* <div className="form-control-feedback" *ngIf="state.errors && (state.dirty || state.touched)">
 
         <span className="text-danger" *ngIf="state.errors.required">State is required</span>
@@ -125,14 +168,17 @@ const RdsCompBillingAddress = (props: RdsCompBillingAddressProps) => {
                 </div > */}
                         </div >
                         <div className="col-md-6 mb-3">
-                            <RdsInput title="Postal Code" titleType="top" 
-                            // isRequired={true} 
-                            // required 
-                            // size="medium"
-                                name="postalCode" id="txtPostalCode"
-                                placeholder="Enter Postal code" 
+                            <RdsInput
+                                label="Postal Code"
+                                titleType="top"
+                                // isRequired={true} 
                                 // required 
-                                />
+                                // size="medium"
+                                name="postalCode"
+                                id="txtPostalCode"
+                                placeholder="Enter Postal code"
+                            // required 
+                            />
                             {/* <div className="form-control-feedback"
                     * ngIf="postalCode.errors && (postalCode.dirty || postalCode.touched)" >
 
@@ -145,15 +191,15 @@ const RdsCompBillingAddress = (props: RdsCompBillingAddressProps) => {
                     <div className="pt-4 row">
                         <div className="col-md-6 mb-3">
                             <RdsButton block={true} size="medium" outlineButton={true} colorVariant="primary"
-                            label='back' tooltipTitle={''} type={'button'}                            // (click)="back()" 
+                                label='back' tooltipTitle={''} type={'button'}                            // (click)="back()" 
                             />
                         </div>
                         <div className="col-md-6 mb-3">
                             <RdsButton block={true} size="medium"
-                            // [disabled]="billingAddressForm.invalid" 
-                            // submit={true}
-                            colorVariant="primary"
-                            label='Continue' tooltipTitle={''} type={'button'} />
+                                // [disabled]="billingAddressForm.invalid" 
+                                // submit={true}
+                                colorVariant="primary"
+                                label='Continue' tooltipTitle={''} type={'submit'} />
                         </div>
                     </div >
                 </form >
