05e0af869bc7258e3f3e0f4f0d01a8087358fbe2
anshuman-singh-wai <112934036+anshuman-singh-wai@users.noreply.github.com>
2022-11-24 11:50:37 +0530

    Removed unnecessary comments from RdsCompTenantList

--- raaghu-mfe/rds_components/src/rds-comp-tenant-list/rds-comp-tenant-list.stories.tsx
+++ raaghu-mfe/rds_components/src/rds-comp-tenant-list/rds-comp-tenant-list.stories.tsx
@@ -13,39 +13,6 @@ const Template: ComponentStory<typeof RdsCompTenantList> = (args) =>
   <RdsCompTenantList {...args} />;
 
 
-// export const Default = Template.bind({});
-
-// Default.args = {
-
-//   tenantSettingInfo: {},
-//   tenantData: {},
-//   tenantList: [
-//     { tenantInfoTemplate: 'Default', statusTemplate: 'Inactive', editionDisplayName: 'Standard', editionTemplate: 'Standard', id: 1 },
-//     { tenantInfoTemplate: '<div class=""><div><div><span>jack</span></div><span class="text-muted">jack </span></div></div>', statusTemplate: '<div> <span class="badge badge-success">Active</span></div>', editionDisplayName: 'Standard', editionTemplate: '<div class="d-flex align-items-center"><div class=…tandard"></div><div class="">Standard</div></div>', id: 4 },
-//     { tenantInfoTemplate: '<div class=""><div><div><span>khalate</span></div><span class="text-muted">ketan </span></div></div>', statusTemplate: '<div><span class="badge badge-secondary">Inactive</span></div>', editionDisplayName: 'Apple1', editionTemplate: '<div class="d-flex align-items-center"><div class=…on Apple1"></div><div class="">Apple1</div></div>', id: 9 },
-//     { tenantInfoTemplate: '<div class=""><div><div><span>pratheesh</span></di… class="text-muted">pratheesh </span></div></div>', statusTemplate: '<div> <span class="badge badge-success">Active</span></div>', editionDisplayName: 'Standard', editionTemplate: '<div class="d-flex align-items-center"><div class=…tandard"></div><div class="">Standard</div></div>', id: 2 },
-//     { tenantInfoTemplate: '<div class=""><div><div><span>richy</span></div><span class="text-muted">richy </span></div></div>', statusTemplate: '<div> <span class="badge badge-success">Active</span></div>', editionDisplayName: 'Standard', editionTemplate: '<div class="d-flex align-items-center"><div class=…tandard"></div><div class="">Standard</div></div>', id: 22 },
-//     { tenantInfoTemplate: '<div class=""><div><div><span>swamiraj</span></div…an class="text-muted">samiraj </span></div></div>', statusTemplate: '<div><span class="badge badge-secondary">Inactive</span></div>', editionDisplayName: 'Standard', editionTemplate: '<div class="d-flex align-items-center"><div class=…tandard"></div><div class="">Standard</div></div>', id: 21 }
-//   ],
-//   editionList: [
-//     { isFree: null, value: '', displayText: 'Not assigned', isSelected: true },
-//     { isFree: true, value: '1', displayText: 'Standard', isSelected: false },
-//     { isFree: false, value: '5', displayText: 'apple', isSelected: false },
-//     { isFree: false, value: '6', displayText: 'Apple1', isSelected: false },
-//   ],
-//   tenantHeaders: [
-//     { displayName: 'Tenant', key: 'tenantInfoTemplate', datatype: 'html', dataLength: 30, sortable: true, required: true, filterable: true },
-//     { displayName: 'Edition', key: 'editionTemplate', datatype: 'html', dataLength: 30, sortable: true, required: true, filterable: true },
-//     { displayName: 'Status', key: 'statusTemplate', datatype: 'html', dataLength: 30, sortable: true, required: true, filterable: true },
-//   ]
-// }
-
-
-
-
-
-
-
 export const Default = Template.bind({});
 
 Default.args = {
