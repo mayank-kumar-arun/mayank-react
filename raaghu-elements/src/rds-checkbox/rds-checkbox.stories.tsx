112bf3adfc63ba21f39cb6786b32904a5f6b765c
saniyasonkaria <saniya.sonkaria@waiin.com>
2022-11-30 13:27:42 +0530

    removed comments
    removed comments

--- raaghu-elements/src/rds-checkbox/rds-checkbox.stories.tsx
+++ raaghu-elements/src/rds-checkbox/rds-checkbox.stories.tsx
@@ -29,35 +29,3 @@ Default.args = {
   errorMessage:"error Message"
 };
 
-
-
-// import React from "react";
-// import { ComponentStory, ComponentMeta } from "@storybook/react";
-// import RdsCheckbox from "./rds-checkbox";
-
-// export default {
-//   title: "Elements/Checkbox",
-//   component: RdsCheckbox,
-//   argTypes: {
-//     state: {
-//       options: ["Checkbox", "Indeterminate", "ErrorCheckbox"],
-//       control: { type: "select" },
-//     },
-//   },
-// } as ComponentMeta<typeof RdsCheckbox>;
-
-// const Template: ComponentStory<typeof RdsCheckbox> = (args) => (
-//   <RdsCheckbox {...args} />
-// );
-
-// export const Default = Template.bind({});
-// Default.args = {
-//   label: "default checkbox",
-//   checked: false,
-//   isDisabled: false,
-//   isSwitch: false,
-//   withLabel: true,
-//   state: "checkbox",
-//   id: "id1",
-//   focus: false,
-// };
