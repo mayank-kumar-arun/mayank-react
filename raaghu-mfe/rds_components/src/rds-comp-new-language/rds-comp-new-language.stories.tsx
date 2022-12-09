8659ff6de7e539685a5e277fdb5f81135686b11e
Yamini Chandana <yamini.chandana@waiin.com>
2022-12-09 17:32:33 +0530

    Improved New Language Component

--- raaghu-mfe/rds_components/src/rds-comp-new-language/rds-comp-new-language.stories.tsx
+++ raaghu-mfe/rds_components/src/rds-comp-new-language/rds-comp-new-language.stories.tsx
@@ -5,34 +5,12 @@ import RdsCompNewLanguage from "./rds-comp-new-language";
 export default {
   title: "Components/New Language",
   component: RdsCompNewLanguage,
-
 } as ComponentMeta<typeof RdsCompNewLanguage>;
 
-
-const Template: ComponentStory<typeof RdsCompNewLanguage> = (args) => 
-  <RdsCompNewLanguage {...args} />;
-
+const Template: ComponentStory<typeof RdsCompNewLanguage> = (args) => (
+  <RdsCompNewLanguage {...args} />
+);
 
 export const Default = Template.bind({});
 
-Default.args ={
-    label:"Open select list",
-  isMultiple:false,
-  selectItems:[
-    {
-      option:"One"
-    },
-    {
-      option:"two"
-    },
-    {
-      option:"three"
-    },
-    {
-      option:"four"
-    }
-
-  ],
-  isDisabled:false
-}
-
+Default.args = {};
--- raaghu-mfe/rds_components/src/rds-comp-new-language/rds-comp-new-language.tsx
+++ raaghu-mfe/rds_components/src/rds-comp-new-language/rds-comp-new-language.tsx
@@ -1,36 +1,45 @@
-import React from 'react'
-import { RdsSelectList } from "../rds-elements";
+import React from "react";
+import { RdsSelectList, RdsCheckbox } from "../rds-elements";
 
-export interface RdsCompNewLanguageProps{
-
-}
+export interface RdsCompNewLanguageProps {}
 const RdsCompNewLanguage = (props: RdsCompNewLanguageProps) => {
   const flags = [
-    {icon: "ad", option: "ad"}
-    ,{icon: "ad", option: "ae"}
-    ,{icon: "ad", option: "af"}
-    ,{icon: "ad", option: "ag"}
-    ,{icon: "ad",option: "ai"}
-];
-const languageNames = [
-  { option: "Invariant Language ()"}
-  ,{option: "Afar (aa)"}
-  ,{ option: "Afar (Djibouti) (aa-DJ)"}
-  ,{ option: "Afar (Eritrea) (aa-ER)"}
-  ,{ option: "Afar (Ethiopia) (aa-ET)"}
-  ,{ option: "Afrikaans (af)"}
-  ,{ option: "Afrikaans (Namibia) (af-NA)"}
-  ,{option: "Afrikaans (South Africa) (af-ZA)"}
-  ,{ option: "Aghem (agq)"}
-  ,{ option: "Aghem (Cameroon) (agq-CM)"}
-  ,{ option: "Akan (ak)"}
-]
+    { option: "ad" },
+    { option: "ae" },
+    { option: "af" },
+    { option: "ag" },
+    { option: "ai" },
+  ];
+  const languageNames = [
+    { option: "Invariant Language ()" },
+    { option: "Afar (aa)" },
+    { option: "Afar (Djibouti) (aa-DJ)" },
+    { option: "Afar (Eritrea) (aa-ER)" },
+    { option: "Afar (Ethiopia) (aa-ET)" },
+    { option: "Afrikaans (af)" },
+    { option: "Afrikaans (Namibia) (af-NA)" },
+    { option: "Afrikaans (South Africa) (af-ZA)" },
+    { option: "Aghem (agq)" },
+    { option: "Aghem (Cameroon) (agq-CM)" },
+    { option: "Akan (ak)" },
+  ];
   return (
     <form>
-    <RdsSelectList label = "Afar (aa)" selectItems = {languageNames} ></RdsSelectList>
-    <RdsSelectList label = "de" selectItems = {flags} ></RdsSelectList>  
-    </form>
-  )
-}
+      <div className="row">
+        <div className="col-md-6 mb-3">
+          <RdsSelectList
+            label="Afar (aa)"
+            selectItems={languageNames}
+          ></RdsSelectList>
+        </div>
+        <div className="col-md-6 mb-3">
+          <RdsSelectList label="de" selectItems={flags}></RdsSelectList>
+        </div>
+      </div>
 
-export default RdsCompNewLanguage
+      <RdsCheckbox label="is Enabled" checked={false}></RdsCheckbox>
+    </form>
+  );
+};
+
+export default RdsCompNewLanguage;
