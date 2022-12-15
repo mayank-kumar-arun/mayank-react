185eb02a4183b52ebef285cca0234affdc6ad6fd
Mayank Kumar Arun <109716282+mayank-kumar-arun@users.noreply.github.com>
2022-12-15 13:07:24 +0530

    Update webpack.config.js

--- raaghu-mfe/rds_pages/host/webpack.config.js
+++ raaghu-mfe/rds_pages/host/webpack.config.js
@@ -13,46 +13,46 @@ const fs = require("fs");
 const mfeFilePath = path.join(__dirname, "../", "mfe-config.ts");
 let mfeConfig = fs.readFileSync(mfeFilePath).toString();
 let mfeConfigJSON = mfeConfig.substring(
-  mfeConfig.indexOf("{"),
-  mfeConfig.lastIndexOf("}") + 1
+	mfeConfig.indexOf("{"),
+	mfeConfig.lastIndexOf("}") + 1
 );
 mfeConfigJSON = JSON.parse(mfeConfigJSON);
 
 module.exports = (env, argv) => {
-  return {
-    entry: "./src/index.ts",
-    mode: "development",
-    devServer: {
-      port: 8080,
-      open: true,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-      hot: true,
-      historyApiFallback: true,
-    },
-    resolve: {
-      extensions: [".ts", ".tsx", ".js"],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(png|jpe?g|gif)$/i,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                regExp: /\/([a-z0-9]+)\/[a-z0-9]+\.png$/i,
-                name: "[1]-[name].[ext]",
-              },
-            },
-          ],
-        },
-        { test: /\.(config)$/, loader: "file-loader" },
-        {
-          test: /\.(scss|css)$/,
+	return {
+		entry: "./src/index.ts",
+		mode: "development",
+		devServer: {
+			port: 8080,
+			open: true,
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+			},
+			hot: true,
+			historyApiFallback: true,
+		},
+		resolve: {
+			extensions: [".ts", ".tsx", ".js"],
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(png|jpe?g|gif)$/i,
+					use: [
+						{
+							loader: "file-loader",
+							options: {
+								regExp: /\/([a-z0-9]+)\/[a-z0-9]+\.png$/i,
+								name: "[1]-[name].[ext]",
+							},
+						},
+					],
+				},
+				{ test: /\.(config)$/, loader: "file-loader" },
+				{
+					test: /\.(scss|css)$/,
 
-          use: ["style-loader", "css-loader", "sass-loader"],
+					use: ["style-loader", "css-loader", "sass-loader"],
 
 					exclude: "/node_modules/",
 				},
@@ -85,18 +85,22 @@ module.exports = (env, argv) => {
 			],
 		},
 
-    plugins: [
-      new webpack.EnvironmentPlugin({ BUILD_DATE: buildDate }),
-      new webpack.DefinePlugin({
-        "process.env": JSON.stringify(process.env),
-      }),
-      new ModuleFederationPlugin({
-        name: "host",
-        remotes: {
-          Dashboard: mfeConfigJSON["dashboard"].url,
-          Login: mfeConfigJSON["login"].url,
-          ForgotPassword: mfeConfigJSON["forgotpassword"].url,
-        },
+		plugins: [
+			new webpack.EnvironmentPlugin({ BUILD_DATE: buildDate }),
+			new webpack.DefinePlugin({
+				"process.env": JSON.stringify(process.env),
+			}),
+			new ModuleFederationPlugin({
+				name: "host",
+				remotes: {
+					Dashboard: mfeConfigJSON["dashboard"].url,
+					Login: mfeConfigJSON["login"].url,
+					ForgotPassword: mfeConfigJSON["forgotpassword"].url,
+				},
+
+				shared: {
+					...devdeps,
+					...deps,
 
 					react: { singleton: true, eager: true, requiredVersion: deps.react },
 					"react-dom": {
@@ -105,7 +109,7 @@ module.exports = (env, argv) => {
 						requiredVersion: deps["react-dom"],
 					},
 				},
-			),
+			}),
 			new CopyWebpackPlugin([{ from: "public/images", to: "dist" }]),
 			new HtmlWebpackPlugin({
 				template: "./public/index.html",
