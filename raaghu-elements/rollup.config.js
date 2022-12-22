c2d77848d2dcdf0feb2047b6be4dda1dff1312bc
Vikrant singh <Singvikrant21@gmail.com>
2022-12-22 11:51:22 +0530

    Made npm package of elements

--- raaghu-elements/rollup.config.js
+++ raaghu-elements/rollup.config.js
@@ -2,6 +2,9 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import babel from "rollup-plugin-babel";
+import external from "rollup-plugin-peer-deps-external";
+import { terser } from "rollup-plugin-terser";
 
 // NEW
 import postcss from "rollup-plugin-postcss";
@@ -21,19 +24,24 @@ export default [
         file: packageJson.module,
         format: "esm",
         sourcemap: true,
+        exports: "named",
       },
     ],
     plugins: [
+      babel({
+        exclude: ["node_modules/**"],
+        presets: ["@babel/preset-typescript"],
+      }),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-
-      // NEW
-      postcss(), 
+      postcss(),
+      external(),
+      terser(),
     ],
   },
   {
-    input: "dist/esm/types/index.d.ts",
+    input: "dist/esm/src/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
 
