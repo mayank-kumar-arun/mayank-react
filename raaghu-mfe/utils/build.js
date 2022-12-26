4dac27d48a2e06d1e71375463c35c59b962e0956
Mayank Kumar Arun <109716282+mayank-kumar-arun@users.noreply.github.com>
2022-12-26 12:52:34 +0530

    Update build.js

--- raaghu-mfe/utils/build.js
+++ raaghu-mfe/utils/build.js
@@ -50,7 +50,7 @@ if (appConfig.replaceUrl == "true") {
           if (portConf == "host") {
             url = url.replace(/((http|https):\/\/localhost:)\d{4}/g, appConfig.appBaseUrl);
           } else {
-              url = url.replace(/((http|https):\/\/localhost:)\d{4}/g, appConfig.appBaseUrl + "/" + pathConf);
+              url = url.replace(/((http|https):\/\/localhost:)\d{4}/g, appConfig.appBaseUrl + "/" + "rds-page-"+ pathConf);
           }
           mfeConfigJSON[mfeConf].url = url;
           break;
