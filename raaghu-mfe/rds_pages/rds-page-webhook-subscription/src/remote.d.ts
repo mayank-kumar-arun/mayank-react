db54966d8b0b02038a747a950b980cb22830a291
saniyasonkaria <saniya.sonkaria@waiin.com>
2022-12-29 17:49:47 +0530

    added Webhook subsctiption page in raaghu-mfe.
     added Webhook subsctiption page in raaghu-mfe.

---
+++ raaghu-mfe/rds_pages/rds-page-webhook-subscription/src/remote.d.ts
@@ -0,0 +1,7 @@
+///<reference types="react" />
+
+declare module "raaghu-components/RdsCompWebhookSubscription" {
+	const WebhookSubscriptionComponent: React.ComponentType;
+
+	export default WebhookSubscriptionComponent;
+}
