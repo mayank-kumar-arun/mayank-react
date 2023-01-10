ccc242c34300978a4c0d9bee89d4fda6e46918c4
Mayank Kumar Arun <109716282+mayank-kumar-arun@users.noreply.github.com>
2023-01-10 17:26:23 +0530

    Update index.ts

--- raaghu-components/src/index.ts
+++ raaghu-components/src/index.ts
@@ -24,4 +24,4 @@ export {default as RdsCompEmail} from "./rds-comp-email/rds-comp-email"
 export {default as RdsCompInvoice} from "./rds-comp-invoice/rds-comp-invoice";
 export {default as RdsCompInformation} from "./rds-comp-information/rds-comp-information";
 export {default as RdsCompLinkedAccount} from "./rds-comp-linked-account/rds-comp-linked-account";
-export {default as RdsCompDynamicEntityProperty} from './rds-comp-dynamic-entity-property/rds-comp-dynamic-entity-property';
+// export {default as RdsCompDynamicEntityProperty} from './rds-comp-dynamic-entity-property/rds-comp-dynamic-entity-property';
