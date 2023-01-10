98f699c5b359dea10337923ddda0cad4063dcb54
Abhishek Gaurav <109716529+agaurav0019@users.noreply.github.com>
2023-01-10 16:59:58 +0530

    Added icon to the boolean chart in dashboard page

--- raaghu-elements/src/rds-chart-boolean/rds-chart-boolean.tsx
+++ raaghu-elements/src/rds-chart-boolean/rds-chart-boolean.tsx
@@ -1,41 +1,70 @@
-import React, { useEffect} from "react";
-import Chart from 'chart.js/auto';
+import React, { useState, useEffect } from "react";
+import Chart from "chart.js/auto";
+import { Buffer } from 'buffer';
+import  {ChartIcons } from "./chart-icons";
 
 export interface RdsBooleanChartProps {
-  labels:any[],
-  options:any,
-  dataSets:any[],
-  width?: number,
-  chartStyle?:string,
-  id:string,
-  height?:number
+  labels: any[];
+  options: any;
+  dataSets: any[];
+  width?: number;
+  chartStyle?: string;
+  id: string;
+  height?: number;
+  centerIconName?:string;
 }
 
 const RdsBooleanChart = (props: RdsBooleanChartProps) => {
- const CanvasId = props.id;
+  const CanvasId = props.id;
   let ctx;
- 
+  
+  
+  const svg = ChartIcons[props.centerIconName || ""];
+  const encodedSVG = Buffer.from(svg).toString('base64');
+  const dataURL = `data:image/svg+xml;base64,${encodedSVG}`;
+
 
   useEffect(() => {
     const canvasElm = document.getElementById(
       CanvasId
     ) as HTMLCanvasElement | null;
     ctx = canvasElm?.getContext("2d") as CanvasRenderingContext2D;
-    
+
+    let centerIcon = {
+      id: "counter4",
+      afterDraw(chart: any) {
+        const ctx = chart.ctx;
+        ctx.save();
+        let myIconImage = new Image();
+        myIconImage.src = dataURL;
+        const iconSize = 50;
+        const x = chart.width / 2 - iconSize / 2;
+        const y = chart.height / 2 - iconSize / 2;
+        ctx.drawImage(myIconImage, x, y, iconSize, iconSize);
+        ctx.restore();
+      },
+    }
+
     const BoolCanvas = new Chart(ctx, {
       type: "doughnut",
+      plugins: [
+        centerIcon
+      ],
       data: {
         labels: props.labels,
-        datasets:props.dataSets
+        datasets: props.dataSets,
       },
       options: props.options,
     });
-      BoolCanvas.canvas.style.height = props.height + 'px';
+    if (BoolCanvas !== null) {
+     
+      BoolCanvas.canvas.style.height = props.height + "px";
       BoolCanvas.canvas.style.width = props.width + "px";
-      return () => {
-        BoolCanvas.destroy()
-      }
-  },[]);
+     }
+    return () => {
+      BoolCanvas.destroy();
+    };
+  }, []);
 
   return (
     <div>
