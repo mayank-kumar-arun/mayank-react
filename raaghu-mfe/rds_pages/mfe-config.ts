c2cdc37ea643f040c432f8221ee0eb969e4c8f8e
saniyasonkaria <saniya.sonkaria@waiin.com>
2023-01-27 17:57:33 +0530

    resolved build error

--- raaghu-mfe/rds_pages/mfe-config.ts
+++ raaghu-mfe/rds_pages/mfe-config.ts
@@ -1,50 +1,50 @@
 export const MfeConfig = {
-  "host": {
-    "url": "https://anzstagereact.raaghu.io/remoteEntry.js"
-  },
-  "edition": {
-    "url": "edition@https://anzstagereact.raaghu.io/rds-page-edition/remoteEntry.js"
-  },
-  "dashboard": {
-    "url": "dashboard@https://anzstagereact.raaghu.io/rds-page-dashboard/remoteEntry.js"
-  },
-  "forgotpassword": {
-    "url": "forgotpassword@https://anzstagereact.raaghu.io/rds-page-forgotpassword/remoteEntry.js"
-  },
-  "login": {
-    "url": "login@https://anzstagereact.raaghu.io/rds-page-login/remoteEntry.js"
-  },
-  "maintainance": {
-    "url": "maintainance@https://anzstagereact.raaghu.io/rds-page-maintainance/remoteEntry.js"
-  },
-  "tenant": {
-    "url": "tenant@https://anzstagereact.raaghu.io/rds-page-tenant/remoteEntry.js"
-  },
-  "visualSetting": {
-    "url": "visualSetting@https://anzstagereact.raaghu.io/rds-page-visualSetting/remoteEntry.js"
-  },
-  "webhookSubscription": {
-    "url": "webhookSubscription@https://anzstagereact.raaghu.io/rds-page-webhookSubscription/remoteEntry.js"
-  },
-  "auditLogs": {
-    "url": "auditlogs@https://anzstagereact.raaghu.io/rds-page-auditLogs/remoteEntry.js"
-  },
-  "settings": {
-    "url": "settings@https://anzstagereact.raaghu.io/rds-page-settings/remoteEntry.js"
-  },
-  "dynamicProperties": {
-    "url": "dynamicProperties@https://anzstagereact.raaghu.io/rds-page-dynamicProperties/remoteEntry.js"
-  },
-  "roles": {
-    "url": "roles@https://anzstagereact.raaghu.io/rds-page-roles/remoteEntry.js"
-  },
-  "organizationUnits": {
-    "url": "organizationUnits@https://anzstagereact.raaghu.io/rds-page-organizationUnits/remoteEntry.js"
-  },
-  "language": {
-    "url": "language@https://anzstagereact.raaghu.io/rds-page-language/remoteEntry.js"
-  },
-  "users": {
-    "url": "users@https://anzstagereact.raaghu.io/rds-page-users/remoteEntry.js"
-  }
+	"host":{
+		 "url":"http://localhost:8080/remoteEntry.js"
+	},
+	"edition":{
+		"url":"edition@http://localhost:8001/remoteEntry.js"
+	},
+	"dashboard":{
+		 "url":"dashboard@http://localhost:8002/remoteEntry.js"
+	},
+	"forgotpassword":{
+		 "url":"forgotpassword@http://localhost:8003/remoteEntry.js"
+	},
+	"login":{
+		 "url":"login@http://localhost:8004/remoteEntry.js"
+	},
+	"maintainance":{
+		"url":"maintainance@http://localhost:8005/remoteEntry.js"
+	},
+	"tenant":{
+		"url":"tenant@http://localhost:8006/remoteEntry.js"
+	},
+	"visualSetting":{
+		"url":"visualSetting@http://localhost:8007/remoteEntry.js"
+	},
+	"webhookSubscription":{
+		"url":"webhookSubscription@http://localhost:8008/remoteEntry.js"
+	},
+	"auditLogs":{
+		"url":"auditlogs@http://localhost:8009/remoteEntry.js" 
+	},
+	"settings":{
+		"url":"settings@http://localhost:8010/remoteEntry.js"
+	},
+	"dynamicProperties":{
+		"url":"dynamicProperties@http://localhost:8011/remoteEntry.js"
+	},
+	"roles":{
+		"url":"roles@http://localhost:8012/remoteEntry.js"
+	},
+	"organizationUnits":{
+		"url":"organizationUnits@http://localhost:8013/remoteEntry.js"
+	},
+	"language":{
+		"url":"language@http://localhost:8014/remoteEntry.js"
+	},
+	"users":{
+		"url":"users@http://localhost:8015/remoteEntry.js"
+	}
 }
